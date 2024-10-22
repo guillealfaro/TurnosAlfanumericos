94904cc14fad8e5e4799481bc20b2eea9ed6d5dd
imolinaa <imolinaa@ucenfotec.ac.cr>
2024-10-21 20:13:00 -0600

    logica .h & .cpp

--- cola.h
+++ cola.h
@@ -10,7 +10,7 @@
 class Cola {
 private:
     static const int MAX_SIZE = 100;  // Tamaño máximo de la cola
-    int datos[MAX_SIZE];              // Arreglo para almacenar los elementos
+    std::string datos[MAX_SIZE];              // Arreglo para almacenar los elementos
     int frente;                       // Índice del frente de la cola
     int final;                        // Índice del final de la cola
     int longitud;                     // Cantidad de elementos en la cola
@@ -18,11 +18,11 @@ private:
 public:
     Cola();                          // Constructora: Construye una cola vacía
     bool esVaciaCola() const;         // Analizadora: Retorna si la cola está vacía
-    Cola insertarElem(const std::string &elemento);  // Modificadora: Inserta un nuevo elemento
-    Cola atender(const std::string &elemento);      // Modificadora: Extrae el primer elemento de la cola
-    Cola eliminarElem(std::string posicion);  // Modificadora: Elimina un elemento por su posición
+    Cola insertarElem(const std::string& turno);  // Modificadora: Inserta un nuevo turno
+    Cola atender(std::string &elemento);      // Modificadora: Extrae el primer elemento de la cola
+    Cola eliminarElem(const std::string &posicion);  // Modificadora: Elimina un elemento por su posición
     std::string ultimoElem() const;           // Analizadora: Retorna el último elemento de la cola
-    std::string getLongitud() const;          // Analizadora: Retorna la cantidad de elementos
+    int getLongitud() const;          // Analizadora: Retorna la cantidad de elementos
     void destruirCola();              // Destructora: Elimina todos los elementos de la cola
 };
 
